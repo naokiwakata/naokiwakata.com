9ae85d8239b886869751079209edd9fa0aefaac9
naokiwakata <rockwakata15@gmail.com>
2024-07-15 21:17:47 +0900

    add TODO

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
+// TODO: ChakraUIでコンポーネントを作成する
+// TODO: OGPを作成する
+
 function MyApp({ Component, pageProps }: { Component: React.ElementType, pageProps: any }) {
     return (
         <ChakraProvider>
